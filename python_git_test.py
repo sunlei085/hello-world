09943a05cc12061d00e083714d9db52d97905d35
benben <sunlei085@gmail.com>
2016-08-21 18:31:48 +0800

    测试git上传

---
+++ python_git_test.py
@@ -0,0 +1,18 @@
+input = ['adam', 'LISA', 'barT']
+
+def transfer(x):
+    list_str = []
+    for num, _ in enumerate(x):
+        if num == 0:
+            list_str.append(_.upper())
+        else:
+            list_str.append(_.lower())
+
+    return ''.join(list_str)
+
+
+print map(lambda x: x.capitalize(), input)
+
+int_input = [1,2 ,3 ,4 ,5, 10]
+
+print reduce(lambda x, y: x*y, int_input)
